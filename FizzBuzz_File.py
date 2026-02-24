19d922ab721b3baf1d38f5d876ef2f0cd62a2528
YueminYan <yy3674@columbia.edu>
2026-02-24 15:42:11 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,5 +1,7 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    v = ['buzz', 41, 'fizz', 43, 44, 'fizzbuzz']
-    return(v)
+    numvec=np.arange(start, finish+1)
+    condition=[(numvec % 15 == 0), (numvec % 3 == 0), (numvec % 5 == 0)]
+    choice=['FizzBuzz', 'Fizz', 'Buzz']
+    return np.select(condition, choice, default=numvec.astype(str))
