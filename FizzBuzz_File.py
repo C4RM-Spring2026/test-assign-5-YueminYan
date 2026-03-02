6e61ac452de793b50cdc277a0a0b0bd648798bf9
YueminYan <yy3674@columbia.edu>
2026-03-01 23:29:29 -0500

    Update FizzBuzz_File.py

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -4,5 +4,5 @@ def FizzBuzz(start, finish):
   numvec=np.arange(start, finish+1)
   objvec = np.array(numvec,dtype = 'object')
   condition=[(numvec % 15 == 0), (numvec % 3 == 0), (numvec % 5 == 0)]
-  choice=['FizzBuzz', 'Fizz', 'Buzz']
-  return np.select(condition, choice, default=numvec.astype(str))
+  choice=["fizzbuzz", "fizz", "buzz"]
+  np.select(condition, choice, default=objvec)
